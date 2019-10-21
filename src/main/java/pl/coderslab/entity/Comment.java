b3944ce67707a7305d56be2a13394aefde60f133
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-21 14:53:41 +0200

    added getters and setters to Comment entity

--- src/main/java/pl/coderslab/entity/Comment.java
+++ src/main/java/pl/coderslab/entity/Comment.java
@@ -28,5 +28,60 @@ public class Comment {
         created = LocalDateTime.now();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public Integer getFlags() {
+        return flags;
+    }
+
+    public void setFlags(Integer flags) {
+        this.flags = flags;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Long getPicture_id() {
+        return picture_id;
+    }
+
+    public void setPicture_id(Long picture_id) {
+        this.picture_id = picture_id;
+    }
+
+    public Long getParent_comment_id() {
+        return parent_comment_id;
+    }
+
+    public void setParent_comment_id(Long parent_comment_id) {
+        this.parent_comment_id = parent_comment_id;
+    }
 }
 
