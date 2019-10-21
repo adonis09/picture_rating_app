67b41cabf6adf0c7f6931e31303035dd3a724951
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-21 14:52:26 +0200

    added constructor, getters and setters to Picture entity

--- src/main/java/pl/coderslab/entity/Picture.java
+++ src/main/java/pl/coderslab/entity/Picture.java
@@ -33,4 +33,54 @@ public class Picture {
         approved = LocalDateTime.now();
     }
 
+    public Picture() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getApproved() {
+        return approved;
+    }
+
+    public void setApproved(LocalDateTime approved) {
+        this.approved = approved;
+    }
+
+    public int getValid() {
+        return valid;
+    }
+
+    public void setValid(int valid) {
+        this.valid = valid;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
 }
