eb3f3acd547a7bc877208c667c959378a8280725
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-22 20:49:15 +0200

    additional methods in PictureDao class

--- src/main/java/pl/coderslab/controller/PictureController.java
+++ src/main/java/pl/coderslab/controller/PictureController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.dao.PictureDao;
+import pl.coderslab.entity.Comment;
 import pl.coderslab.entity.Picture;
+import pl.coderslab.entity.Tag;
+
+import java.util.List;
 
 @RequestMapping("/picture")
 @Controller
@@ -52,6 +56,98 @@ public class PictureController {
         pictureDao.delete(picture);
 
         return "Deleted picture having id: " + picture.getId();
-
     }
+
+    @RequestMapping("/find-unapproved")
+    @ResponseBody
+    public String findAllUnapproved(){
+
+        List<Picture> unapprovedPictures = pictureDao.findAllUnapprovedPictures();
+        String returnString = new String();
+        for (Picture onePicture : unapprovedPictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/find-approved")
+    @ResponseBody
+    public String findAllApproved(){
+
+        List<Picture> unapprovedPictures = pictureDao.findAllApprovedPictures();
+        String returnString = new String();
+        for (Picture onePicture : unapprovedPictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/find-approved-of-user/{id}")
+    @ResponseBody
+    public String findAllApprovedOfUser(@PathVariable("id") long id){
+
+        List<Picture> unapprovedPictures = pictureDao.findAllApprovedPicturesOfUser(id);
+        String returnString = new String();
+        for (Picture onePicture : unapprovedPictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/find-all-unmarked-by-user/{id}")
+    @ResponseBody
+    public String findUnmarkedByUser(@PathVariable("id") long id){
+
+        List<Picture> unapprovedPictures = pictureDao.findAllUnmarkedPicturesByUser(id);
+        String returnString = new String();
+        for (Picture onePicture : unapprovedPictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/find-all-having-tag/{id}")
+    @ResponseBody
+    public String findAllHavingTag(@PathVariable("id") long id){
+
+        List<Picture> unapprovedPictures = pictureDao.findAllPicturesHavingTag(id);
+        String returnString = new String();
+        for (Picture onePicture : unapprovedPictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/find-all-tags-on/{id}")
+    @ResponseBody
+    public String findAllTagsOn(@PathVariable("id") long id){
+
+        List<Tag> unapprovedPictures = pictureDao.findAllTagsOn(id);
+        String returnString = new String();
+        for (Tag oneTag : unapprovedPictures) {
+            returnString += oneTag;
+        }
+        return returnString;
+    }
+
+    @RequestMapping("/calculate-avg-score-of/{id}")
+    @ResponseBody
+    public String calculateAvgScore(@PathVariable("id") long id){
+
+        Double avgScore = pictureDao.calculateAvgScoreOf(id);
+        return "Average score: " + avgScore;
+    }
+
+    @RequestMapping("/find-all-order-by-score")
+    @ResponseBody
+    public String findAllOrderByAvgScore(){
+
+        List<Picture> pictures = pictureDao.findAllOrderByAvgScore();
+        String returnString = new String();
+        for (Picture onePicture : pictures) {
+            returnString += onePicture;
+        }
+        return returnString;
+    }
+
 }
--- src/main/java/pl/coderslab/dao/PictureDao.java
+++ src/main/java/pl/coderslab/dao/PictureDao.java
@@ -3,6 +3,7 @@ package pl.coderslab.dao;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.entity.Picture;
+import pl.coderslab.entity.Tag;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,6 +16,7 @@ public class PictureDao {
 
     @PersistenceContext
     EntityManager entityManager;
+
     public void savePicture(Picture entity) {
         entityManager.persist(entity);
     }
@@ -32,10 +34,63 @@ public class PictureDao {
                 entity : entityManager.merge(entity));
     }
 
-    public List<Picture> findAll(){
+    public List<Picture> findAll() {
         Query query = entityManager.createQuery("SELECT p FROM Picture p");
         List<Picture> pictures = query.getResultList();
         return pictures;
+    }
+
+    public List<Picture> findAllUnapprovedPictures() {
+        Query query = entityManager.createQuery("SELECT p FROM Picture p WHERE p.valid = 0");
+        List<Picture> unapprovedPictures = query.getResultList();
+        return unapprovedPictures;
+    }
+
+    public List<Picture> findAllApprovedPictures() {
+        Query query = entityManager.createQuery("SELECT p FROM Picture p WHERE p.valid = 1");
+        List<Picture> unapprovedPictures = query.getResultList();
+        return unapprovedPictures;
+    }
+
+    public List<Picture> findAllApprovedPicturesOfUser(Long userId) {
+        Query query = entityManager.createQuery("SELECT p FROM Picture p WHERE p.valid = 1 AND p.user.id = :userId");
+        query.setParameter("userId", userId);
+        List<Picture> unapprovedPictures = query.getResultList();
+        return unapprovedPictures;
+    }
+
+    public List<Picture> findAllUnmarkedPicturesByUser(Long userId) {
+        Query query = entityManager.createQuery("SELECT p FROM Picture p JOIN p.marks m WHERE m.user.id <> :userId AND p.valid = 1");
+        query.setParameter("userId", userId);
+        List<Picture> unapprovedPictures = query.getResultList();
+        return unapprovedPictures;
+    }
+
+    public List<Picture> findAllPicturesHavingTag(Long tagId) {
+        Query query = entityManager.createQuery("SELECT p FROM Picture p JOIN p.tags t WHERE t.id = :tagId");
+        query.setParameter("tagId", tagId);
+        List<Picture> picturesWithTag = query.getResultList();
+        return picturesWithTag;
+    }
+
+    public List<Tag> findAllTagsOn(Long pictureId) {
+        Query query = entityManager.createQuery("SELECT t FROM Tag t JOIN t.pictures p WHERE p.id = :pictureId");
+        query.setParameter("pictureId", pictureId);
+        List<Tag> allTagsOnPicture = query.getResultList();
+        return allTagsOnPicture;
+    }
+
+    public Double calculateAvgScoreOf(Long pictureId) {
+        Query query = entityManager.createQuery("SELECT AVG (m.score) FROM Mark m WHERE m.picture.id = :pictureId");
+        query.setParameter("pictureId", pictureId);
+        Double result = Double.parseDouble(query.getSingleResult().toString());
+        return result;
+    }
+
+    public List<Picture> findAllOrderByAvgScore(){
+        Query query = entityManager.createQuery("SELECT p FROM Picture p LEFT JOIN p.marks m GROUP BY p ORDER BY AVG(m.score) DESC");
+        List<Picture> allPicturesOrderByAvgScore = query.getResultList();
+        return allPicturesOrderByAvgScore;
     }
 
 }
